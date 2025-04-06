a96641a59733a55ae4029e9bcd9ecd4df2149821
Roverp <yar.zubaha@proton.me>
2025-04-06 17:31:58 +0200

    refactor(main): -> split code into funcitons

--- timer.c
+++ timer.c
@@ -8,30 +8,48 @@
 
 struct termios orig_termios;
 
-void enable_raw_input();
-
+int parse_time(const char *arg);
+void run_timer(int total_seconds);
+void print_intro();
 void restore_terminal();
+void enable_raw_input();
+void enable_non_blocking_input();
 
 int main(int argc, char *argv[]) {
-  int hours = 0;
-  int minutes = 0;
-  int seconds = 0;
-
-  int num = 0;
-  int total_seconds = hours * 3600 + minutes * 60 + seconds;
-
-  int stdin_flags = fcntl(STDIN_FILENO, F_GETFL);
-  int non_blocking_stdin_flags = stdin_flags | O_NONBLOCK;
-
-  bool pause_flag = false;
+  int total_seconds;
 
   if (argc < 2) {
     printf("Not enough arguments: usage of %s\n", argv[0]);
     return 1;
   }
 
-  for (int i = 0; argv[1][i] != '\0'; i++) {
-    char c = argv[1][i];
+  total_seconds = parse_time(argv[1]);
+
+  if (total_seconds <= 0) {
+    printf("Provide time that is greater than 0\nIn format: 3h2m1s\n");
+    return 1;
+  }
+
+  enable_non_blocking_input();
+  enable_raw_input();
+  atexit(restore_terminal);
+
+  print_intro();
+  run_timer(total_seconds);
+  printf("\n\nTime's up 󱫑\n");
+
+  return 0;
+}
+
+int parse_time(const char *arg) {
+  int hours = 0;
+  int minutes = 0;
+  int seconds = 0;
+  int num = 0;
+  int total_seconds;
+
+  for (int i = 0; arg[i] != '\0'; i++) {
+    char c = arg[i];
 
     if (isdigit(c)) {
       num = num * 10 + (c - '0');
@@ -46,23 +64,45 @@ int main(int argc, char *argv[]) {
       num = 0;
     } else {
       printf("Error while parsing %c\n", c);
-      return 1;
+      return -1;
     }
   }
 
   total_seconds = hours * 3600 + minutes * 60 + seconds;
 
-  if (total_seconds <= 0) {
-    printf("Provide time that is greater than 0\nIn format: 3h2m1s");
-    return 1;
-  }
+  return total_seconds;
+}
+
+void enable_raw_input() {
+  struct termios new_termios;
+
+  // get current terminal settings
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  new_termios = orig_termios;
+
+  // disable line buffering n echo mode
+  new_termios.c_lflag &= ~(ICANON | ECHO);
+
+  // set terminal settings
+  tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
+  printf("\033[?25l");
+}
+
+void enable_non_blocking_input() {
+  int stdin_flags = fcntl(STDIN_FILENO, F_GETFL);
+  int non_blocking_stdin_flags = stdin_flags | O_NONBLOCK;
 
   fcntl(STDIN_FILENO, F_SETFL, non_blocking_stdin_flags);
-  enable_raw_input();
-  atexit(restore_terminal);
+}
 
+void print_intro() {
   printf("Timer started (press 'p' to pause)\n\n");
   printf("Time left:\n");
+}
+
+void run_timer(int total_seconds) {
+  bool pause_flag = false;
+
   while (total_seconds >= 0) {
     int h = total_seconds / 3600;
     int m = (total_seconds % 3600) / 60;
@@ -94,25 +134,6 @@ int main(int argc, char *argv[]) {
     fflush(stdout);
     sleep(1);
   }
-
-  printf("\n\nTime's up 󱫑\n");
-
-  return 0;
-}
-
-void enable_raw_input() {
-  struct termios new_termios;
-
-  // get current terminal settings
-  tcgetattr(STDIN_FILENO, &orig_termios);
-  new_termios = orig_termios;
-
-  // disable line buffering n echo mode
-  new_termios.c_lflag &= ~(ICANON | ECHO);
-
-  // set terminal settings
-  tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
-  printf("\033[?25l");
 }
 
 void restore_terminal() {
